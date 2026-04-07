65c01edd00f3e4a17e454ff1574cd24a88a5d0db
loyage <792058350@qq.com>
2026-04-07 12:37:59 +0800

    delete nix-yazi-flavors

--- home/terminal/programs/catppuccin.nix
+++ home/terminal/programs/catppuccin.nix
@@ -1,12 +1,11 @@
 { ... }: {
   catppuccin.enable = true;
-  catppuccin.flavor = "mocha";
+  catppuccin.flavor = "frappe";
   catppuccin.accent = "blue";
 
   # Disable catppuccin for programs that have custom/symlinked themes to avoid conflicts
   catppuccin.lazygit.enable = false;
   catppuccin.nvim.enable = false;
-  catppuccin.yazi.enable = false;
 
   programs.bat.enable = true;
   programs.fzf.enable = true;
