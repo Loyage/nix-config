86f0f90457bc47e947070111cf7dc4ffc3ccbd37
loyage <792058350@qq.com>
2026-06-07 19:51:04 +0800

    minimize last fix

--- modules/linux/clash-verge.nix
+++ modules/linux/clash-verge.nix
@@ -12,11 +12,4 @@
     tunMode = true;
     group = myvars.username;
   };
-
-  # networking.firewall.trustedInterfaces = [ "Mihomo" "Meta" ];
-
-  # boot.kernel.sysctl = {
-  #   "net.ipv4.ip_forward" = 1;
-  #   "net.ipv6.conf.all.forwarding" = 1;
-  # };
 }
