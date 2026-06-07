007942bebcd61b42856788efdb79d79eaaeb7cf1
loyage <792058350@qq.com>
2026-06-07 19:50:50 +0800

    fix clash-verge TUN

--- modules/linux/clash-verge.nix
+++ modules/linux/clash-verge.nix
@@ -1,13 +1,22 @@
 { pkgs
+, myvars
 , ...
 }:
 {
+  boot.kernelModules = [ "tun" ];
+
   programs.clash-verge = {
     enable = true;
     package = pkgs.clash-verge-rev;
     serviceMode = true;
     tunMode = true;
+    group = myvars.username;
   };
 
-  networking.firewall.trustedInterfaces = [ "Mihomo" "Meta" ];
+  # networking.firewall.trustedInterfaces = [ "Mihomo" "Meta" ];
+
+  # boot.kernel.sysctl = {
+  #   "net.ipv4.ip_forward" = 1;
+  #   "net.ipv6.conf.all.forwarding" = 1;
+  # };
 }
