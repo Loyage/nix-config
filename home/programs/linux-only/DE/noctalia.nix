a228df7540db93373e5c12f3b0ce0da79767eae8
loyage <792058350@qq.com>
2026-07-06 15:25:09 +0800

    noctalia's idle

--- home/programs/linux-only/DE/noctalia.nix
+++ home/programs/linux-only/DE/noctalia.nix
@@ -57,6 +57,25 @@
         mode = "auto";
         source = "wallpaper";
       };
+      idle = {
+        behavior = {
+          lock = {
+            action = "lock";
+            enabled = true;
+            timeout = 600.0;
+          };
+          lock-and-suspend = {
+            action = "lock_and_suspend";
+            enabled = false;
+            timeout = 900.0;
+          };
+          screen-off = {
+            action = "screen_off";
+            enabled = true;
+            timeout = 660.0;
+          };
+        };
+      };
 
       #     plugins = {
       #       sources = [
