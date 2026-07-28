7bc17cbf25a93d4479d441c46ec22cbd882173a5
loyage <792058350@qq.com>
2026-07-28 16:27:58 +0800

    noctalia session

--- home/programs/linux-only/DE/noctalia.nix
+++ home/programs/linux-only/DE/noctalia.nix
@@ -1,7 +1,5 @@
 { inputs
-, lib
 , pkgs
-, config
 , ...
 }:
 {
@@ -78,6 +76,47 @@
           };
         };
       };
+      shell.session.actions = [
+        {
+          action = "lock";
+          shortcut = "1";
+        }
+        {
+          action = "command";
+          label = "黑屏";
+          glyph = "screen-share-off";
+          command = "noctalia msg dpms-off";
+          shortcut = "2";
+        }
+        {
+          action = "logout";
+          shortcut = "3";
+        }
+        # {
+        #   action = "lock_and_suspend";
+        #   countdown_seconds = 0.0;
+        # }
+        {
+          action = "reboot";
+          countdown_seconds = 10.0;
+          shortcut = "4";
+        }
+        {
+          action = "shutdown";
+          countdown_seconds = 10.0;
+          shortcut = "5";
+          variant = "destructive";
+        }
+        {
+          action = "command";
+          label = "休眠";
+          glyph = "hibernate";
+          command = "sudo systemctl hibernate";
+          countdown_seconds = 10.0;
+          shortcut = "6";
+          variant = "destructive";
+        }
+      ];
 
       #     plugins = {
       #       sources = [
