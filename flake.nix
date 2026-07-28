3e0b6977938bc43fe7fdc4fa976ee77be25e6e60
loyage <792058350@qq.com>
2026-07-28 11:33:18 +0800

    wechat fix

--- flake.nix
+++ flake.nix
@@ -155,6 +155,16 @@
                     hash = "sha256-AvZ3/rHOAe0pOjx3YeXdhb15k29X3KpM21MXiuMOPW0=";
                   };
                 });
+
+                wechat = final.callPackage (inputs.nixpkgs-unstable + "/pkgs/by-name/we/wechat/linux.nix") {
+                  pname = "wechat";
+                  version = "4.1.1.4";
+                  src = final.fetchurl {
+                    url = "https://dldir1v6.qq.com/weixin/Universal/Linux/WeChatLinux_x86_64.AppImage";
+                    hash = "sha256-RX26ArkbAxzdRBLu4HT7v/udnQax5Q/Bgi00hw4RSZA=";
+                  };
+                  meta = prev.wechat.meta;
+                };
               })
               # (final: prev: {
               #   fcitx5-vinput = final.callPackage ./pkgs/fcitx5-vinput.nix { inherit inputs; };
