fdf6156786e9a88cef8327498e80e68eb8b769f9
loyage <792058350@qq.com>
2026-08-19 21:24:08 +0800

    fix(home-manager): remove .npmrc from declarative management
    Home Manager creates .npmrc as a symlink into the read-only Nix store,
    which causes EROFS errors when npm tries to write auth tokens.
    
    npm now manages ~/.npmrc natively.

--- home/programs/core-tools/dev.nix
+++ home/programs/core-tools/dev.nix
@@ -16,10 +16,7 @@
     gcc
   ];
 
-  home.file.".npmrc".text = ''
-    prefix=${config.home.homeDirectory}/.npm
-    min-release-age=7
-  '';
+
   xdg.configFile."pip/pip.conf".text = ''
     [global]
     index-url = https://mirror.nju.edu.cn/pypi/web/simple
